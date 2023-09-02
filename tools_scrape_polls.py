0172fca0319cfd2e1ddb60523fb2ca699f18fbc3
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-09-02 20:50:31 +0200

    mv plot func to separate script in plots dir

--- tools_scrape_polls.py
+++ tools_scrape_polls.py
@@ -138,19 +138,3 @@ def export_dfs_to_csv(df_data, df_trends):
     df_data.to_csv('./polls.csv', index=False)
     df_trends.to_csv('./trends.csv', index=True) # date is index!
 
-def plot_trends_polls(df_trends, df_data, names_candidates, ylim = [-0.05, 0.6]):
-    fig, ax = plt.subplots()
-    colors_plot = ['blue', 'red', 'green', 'orange', 'purple', 'brown']
-    counter_colors = 0
-    for col in df_trends.columns:
-        if col in names_candidates:
-            ax.plot(df_trends.index, df_trends[col], color = colors_plot[counter_colors],
-                    label = col, linestyle = 'solid', linewidth=2)
-            ax.scatter(df_data['Date'], df_data[col], 
-                    marker='o', s = 12, alpha = 0.5, color = colors_plot[counter_colors])
-            counter_colors += 1
-
-    ax.legend(loc='best', ncol=2, fontsize=10, frameon=False)
-    ax.set_ylim(ylim)
-    ax.set_title('Vote shares and trends')
-    fig.savefig('plot_trends.png', dpi=300)
