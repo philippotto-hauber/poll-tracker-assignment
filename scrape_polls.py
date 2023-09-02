ba12e84edbd2c82b6602fdafa9b0fefc40d45073
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-09-02 22:13:22 +0200

    update script
    - add try except blocks for remaining sections
    - re-route warnings to log file
    - define error message

--- scrape_polls.py
+++ scrape_polls.py
@@ -8,7 +8,10 @@ logging.basicConfig(filename = "scrape_polls.log",
                     format = '%(levelname)s: %(asctime)s %(message)s',
                     filemode = 'w')
 
+logging.captureWarnings(True) # re-routes warnings to log file
 logging.info("Begin execution")
+
+error_msg = 'Script terminated with error. Check scrape_polls.log for details.'
 
 #%% scrape table and footnotes from website
 url = 'https://cdn-dev.economistdatateam.com/jobs/pds/code-test/index.html'
@@ -18,7 +21,7 @@ try:
     logging.info("Sucessfully scraped content from url!")
 except Exception as e:
     logging.error(e, exc_info=True)
-    print("Booboo")
+    print(error_msg)
     sys.exit(1)
 
 
@@ -31,26 +34,37 @@ n_candidates = len(names_candidates)
 names_candidates_and_others = [col for col in name_cols if col not in ['Date', 'Pollster', 'Sample']]
 
 #%% parse data
-df_data = parse_data(table, 
+try:
+    df_data = parse_data(table, 
                     name_cols, 
                     names_candidates_and_others, 
                     footnotes, 
                     lims_sum_shares = [0.98, 1.02])
+    logging.info('Sucessfully parsed data!')
+except Exception as e:
+    logging.error(e, exc_info=True)
+    print(error_msg)
+    sys.exit(1)
 
 #%% calculate trends
-df_trends = calculate_trends(df_data, 
-                             names_candidates,
-                             k_days= '7D',
-                             method_interpolate='linear')
+try:
+    df_trends = calculate_trends(df_data, 
+                                 names_candidates,
+                                 k_days= '7D',
+                                 method_interpolate='linear')
+    logging.info('Sucessfully calculated trends!')
+except Exception as e:
+    logging.error(e, exc_info=True)
+    print(error_msg)
+    sys.exit(1)
 
 #%% export to csv
 
 try:
     export_dfs_to_csv(df_data, df_trends)
-    logging.info("Script terminated successfully. Data exported to csv.")
+    logging.info('Script terminated successfully. Data exported to csv. Check scrape_polls.log for possible warnings.')
 except Exception as e:
     logging.error(e, exc_info=True)
-    print("Booboo")
+    print(error_msg)
     sys.exit(1)
-
 
