c2071c6baebe131ce24c4123b5e934576ed5e887
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2023-09-02 17:33:48 +0200

    add try except blocks

--- scrape_polls.py
+++ scrape_polls.py
@@ -1,5 +1,6 @@
 #%% load packages
 from tools_scrape_polls import *
+import sys
 
 #%% set up logging
 logging.basicConfig(filename = "scrape_polls.log", 
@@ -12,7 +13,14 @@ logging.info("Begin execution")
 #%% scrape table and footnotes from website
 url = 'https://cdn-dev.economistdatateam.com/jobs/pds/code-test/index.html'
 
-table, footnotes = scrape_table_and_footnotes(url)
+try:
+    table, footnotes = scrape_table_and_footnotes(url)
+    logging.info("Sucessfully scraped content from url!")
+except Exception as e:
+    logging.error(e, exc_info=True)
+    print("Booboo")
+    sys.exit(1)
+
 
 #%% define a few variables
 name_cols = [col.text.strip() for col in table.find_all("th")]
@@ -30,9 +38,14 @@ df_data = parse_data(table,
                     footnotes, 
                     lims_sum_shares = [0.98, 1.02])
 
-# df_data.head()
-# df_data.dtypes
-# df_data.info(show_counts=True)
+# convert df to html table
+df_data_html = df_data.to_html()
+
+# export mto test.html
+with open("test.html", "w") as file:
+    file.write(df_data_html)
+
+# show pwd
 
 #%% calculate trends
 df_trends = calculate_trends(df_data, 
@@ -46,7 +59,12 @@ plot_trends_polls(df_trends, df_data, names_candidates)
 
 #%% export to csv
 
-export_dfs_to_csv(df_data, df_trends)
+try:
+    export_dfs_to_csv(df_data, df_trends)
+    logging.info("Script terminated successfully. Data exported to csv.")
+except Exception as e:
+    logging.error(e, exc_info=True)
+    print("Booboo")
+    sys.exit(1)
 
-logging.info("Finished scraping and exporting data")
 
